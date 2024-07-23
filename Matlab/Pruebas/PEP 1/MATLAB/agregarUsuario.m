6874db01e83832b5c188a0c7d61988889de8d483
Renato Vásquez <rena_vasquez@icloud.com>
2024-07-22 20:10:21 -0400

    Delete Matlab/Pruebas/PEP 1 directory

--- Matlab/Pruebas/PEP 1/MATLAB/agregarUsuario.m
+++
@@ -1,8 +0,0 @@
-function agregarUsuario()
-    global usuarios
-    usuario.rut = input("Ingrese el RUT del usuario: ", "s");
-    usuario.nombre = input("Ingrese el nombre del usuario: ", "s");
-    usuario.estado = str2double(input("Ingrese el estado del usuario (1:activo , 0:inactivo): ", "s"));
-    usuario.fecha_activacion = input("Ingrese la fecha de activación (dd-mm-yyyy): ", "s");
-    usuarios = [usuarios; usuario];
-end
--- Matlab/Pruebas/PEP 1/MATLAB/grabarDatos.m
+++
@@ -1,6 +0,0 @@
-function grabarDatos()
-    global libros prestamos usuarios
-    save('libros.mat', 'libros');
-    save('prestamos.mat', 'prestamos');
-    save('usuarios.mat', 'usuarios');
-end
